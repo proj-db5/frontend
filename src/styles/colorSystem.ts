09e3646adc03bf324354317e8acc40c39ed5cfc0
hyundang <hyunjin5697@yonsei.ac.kr>
2021-11-22 01:58:23 +0900

    fix: colorSystem

--- src/styles/colorSystem.ts
+++ src/styles/colorSystem.ts
@@ -2,22 +2,22 @@ import { css } from "styled-components";
 
 const ColorSystem = css`
   :root {
-    --black: "#000000";
-    --black_1: "#2C2E37";
-    --white: "#FFFFFF";
-    --gray_1: "#F6F6F6";
-    --gray_2: "#EAEAEB";
-    --gray_3: "#DEE0E3";
-    --gray_4: "#D7D9DD";
-    --gray_6: "#B4B6BC";
-    --gray_7: "#A3A6AE";
-    --gray_8: "#70727C";
-    --gray_9: "#505259";
-    --skyblue_1: "#4BACCB";
-    --skyblue_2: "#0B97B6";
-    --skyblue_3: "#00809C";
-    --skyblue_4: "#045274";
-    --orange: "#FEB287";
+    --black: #000000;
+    --black_1: #2c2e37;
+    --white: #ffffff;
+    --gray_1: #f6f6f6;
+    --gray_2: #eaeaeb;
+    --gray_3: #dee0e3;
+    --gray_4: #d7d9dd;
+    --gray_6: #b4b6bc;
+    --gray_7: #a3a6ae;
+    --gray_8: #70727c;
+    --gray_9: #505259;
+    --skyblue_1: #4baccb;
+    --skyblue_2: #0b97b6;
+    --skyblue_3: #00809c;
+    --skyblue_4: #045274;
+    --orange: #feb287;
   }
 `;
 
