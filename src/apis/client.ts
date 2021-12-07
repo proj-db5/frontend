c52de410b92567b46771d8e56913affe8b383629
hyundang <hyunjin5697@yonsei.ac.kr>
2021-12-08 00:46:26 +0900

    feat: withCredentials 추가

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -2,6 +2,10 @@ import axios from "axios";
 
 const client = axios.create({
   baseURL: API_DOMAIN,
+  // headers: {
+  //   Credential: "include",
+  // },
+  withCredentials: true,
 });
 
 export default client;
